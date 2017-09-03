2ee97d66e6bc7cc1c9c78b7469178278d1cb1ac0
Wiwit Rifa'i <wiwitrifai@gmail.com>
2017-09-03 15:20:22 +0700

    UMN Programming Contest 2017 Preliminary Round

---
+++ indonesian-local/umn17-qual/g.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  int t;
+  scanf("%d", &t);
+  for (int tc = 1; tc <= t; tc++) {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int k = (n-1 + m - 1)/m;
+    printf("Case #%d: %d %d\n", tc, k, n-k);
+  }
+  return 0;
+}
---
+++ indonesian-local/umn17-qual/h.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  int t;
+  scanf("%d", &t);
+  for (int tc = 1; tc <= t; tc++) {
+    double p, d, i;
+    cin >> d >> p >> i;
+    i += 100;
+    i /= 100;
+    int ans = 0;
+    while (d < p) {
+      d *= i;
+      ans++;
+    }
+    printf("Case #%d: %d\n", tc, ans);
+  }
+  return 0;
+}
