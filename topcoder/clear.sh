77ac1bb025bd1ea5e5a4a1c36fa28d11ef8cce7f
Wiwit Rifa'i <wiwitrifai@gmail.com>
2019-08-24 01:24:37 +0700

    Add folder archive for topcoder problems

--- topcoder/clear.sh
+++ topcoder/clear.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 for txt in `ls *.txt`; do
-    echo remove: $txt
+    echo - $txt
     rm $txt
+    source_code="${txt%.*}.cpp"
+    echo "  $source_code -> archive"
+    mv $source_code 'arcive'
 done
-echo "All .txt removed"
+echo "All done!"
