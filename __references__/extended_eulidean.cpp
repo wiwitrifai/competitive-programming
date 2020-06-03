e4ab9784e2150de5c211f2ed505019a01ed48564
Wiwit Rifa'i <wiwitrifai@gmail.com>
2020-06-03 15:56:18 +0700

    Move my reference codes to another repository

--- __references__/extended_eulidean.cpp
+++
@@ -1,9 +0,0 @@
-long long x, y, d; // ax + by = d
-void extendedEuclidean(long long a, long long b) {
-  if(b == 0) { x = 1; y = 0; d = a; return; } 
-  extendedEuclidean(b, a % b); 
-  long long xx, yy;
-  xx = y;
-  yy = x - (a/b)*y;
-  x = xx; y = yy;
-}
--- __references__/fast_mod.cpp
+++
@@ -1,2 +0,0 @@
-#define MM 2305842993ll // MM = 2^61/mod
-inline int rem(long long a){return a-mod*((a>>29)*MM>>32);}
